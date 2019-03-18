90e7df7525e3cbf7d3256a78012cbb5f8da30458
Ricardo <cr.calle@uniandes.edu.co>
2019-03-17 19:00:12 -0500

    Status de respuesta de lista de imágenes (ROJO)

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -8,3 +8,7 @@ import json
 # Create your tests here.
 class GalleryTestCase(TestCase):
 
+    def test_list_images_status(self):
+        url = '/gallery/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)

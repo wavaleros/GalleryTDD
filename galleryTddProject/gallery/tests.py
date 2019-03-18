38b81015ce367cbe13e48b378c4753946572637a
Ricardo <cr.calle@uniandes.edu.co>
2019-03-17 19:12:19 -0500

    Cantidad de elementos de la lista de imágenes (ROJO)

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -12,3 +12,13 @@ class GalleryTestCase(TestCase):
         url = '/gallery/'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
+
+    def test_count_images_list(self):
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test', last_name='test', email='test@test.com')
+        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model)
+
+        response=self.client.get('/gallery/')
+        current_data=json.loads(response.content)
+
+        self.assertEqual(len(current_data),2)

3cdf41b6dfac9458c7306d51565b8ec0fbf4a804
wavaleros <wavaleros@gmail.com>
2020-03-29 15:18:41 -0500

    Agregando el objeto de portafolio

--- galleryTddProject/gallery/models.py
+++ galleryTddProject/gallery/models.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class PortfolioCollection(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Image(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000, null=True)
     type = models.CharField(max_length=5, blank=True)
     user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+    portfolio = models.ForeignKey(PortfolioCollection, null=True, on_delete=models.SET(None))
